1a4e522a37d727adb27723ef5df2123ad0dc98a0
ZeroXKiritsu <kalen795@gmail.com>
2020-03-18 23:40:47 +0300

    Update singleton to acquire-release semantic singleton version

--- shared/detail/singleton.hpp
+++ shared/detail/singleton.hpp
@@ -15,9 +15,23 @@ public:
 public:
 	static T& Instance()
 	{
-		static T object = { };
+		static T object = m_instance.load(std::memory_order_acquire);
+		if ( !object )
+		{
+			std::lock_guard<std::mutex> lock(mutex);
+			object = m_instance.load(std::memory_order_relaxed);
+			if( !object )
+			{
+				object = new Singleton();
+				m_instance.store(object,std::memory_order_release);
+			}
+		}
+		
 		return object;
 	}
+private:
+	static std::atomic<Singleton*> m_instance;
+	static std::mutex mutex;
 };
 
 }
